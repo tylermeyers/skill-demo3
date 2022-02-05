98eb5528be4a207b5bb369f99e1c488c251ccb29
tylermeyers <88351548+tylermeyers@users.noreply.github.com>
2022-02-04 16:04:18 -0800

    Update SkillDemo3Test.java

--- SkillDemo3Test.java
+++ SkillDemo3Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo3Test {
     @Test
     public void minus(){
-        assertEquals(2,SkillDemo3.minus(5,2));
+        assertEquals(2,SkillDemo3.minus(4,2));
     }
 }
